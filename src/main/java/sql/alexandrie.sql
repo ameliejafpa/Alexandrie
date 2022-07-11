e21f1cc7bb31ff52f396064eb9a626b443991d66
Guillaume Andreux <guillaume@andreux.fr>
2022-07-11 13:58:48 +0200

    ajout de la bdd

---
+++ src/main/java/sql/alexandrie.sql
@@ -0,0 +1,590 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : ven. 08 juil. 2022 à 10:33
+-- Version du serveur :  8.0.29-0ubuntu0.20.04.3
+-- Version de PHP : 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `alexandrie`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `administrateur`
+--
+
+CREATE TABLE `administrateur` (
+  `id` int NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `motDePasse` varchar(255) NOT NULL,
+  `privilege` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `adresseLivraison`
+--
+
+CREATE TABLE `adresseLivraison` (
+  `id` int NOT NULL,
+  `idUtilisateur` int NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `codePostal` int NOT NULL,
+  `ville` varchar(255) NOT NULL,
+  `pays` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id` int NOT NULL,
+  `titre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+CREATE TABLE `commande` (
+  `id` int NOT NULL,
+  `idUtilisateur` int NOT NULL,
+  `dateC` date NOT NULL,
+  `total` float NOT NULL,
+  `idAdresse` int NOT NULL,
+  `etat` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaire`
+--
+
+CREATE TABLE `commentaire` (
+  `id` int NOT NULL,
+  `commentaire` varchar(255) NOT NULL,
+  `note` int NOT NULL,
+  `idProduit` int NOT NULL,
+  `idUtilisateur` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int NOT NULL,
+  `idUtilisateur` int NOT NULL,
+  `sujet` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `etat` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `coordonnee`
+--
+
+CREATE TABLE `coordonnee` (
+  `id` int NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `telephone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `logo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `detailsCommande`
+--
+
+CREATE TABLE `detailsCommande` (
+  `id` int NOT NULL,
+  `idCommande` int NOT NULL,
+  `idProduit` int NOT NULL,
+  `quantite` int NOT NULL,
+  `prix` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `entreeEnStock`
+--
+
+CREATE TABLE `entreeEnStock` (
+  `id` int NOT NULL,
+  `idFournisseur` int NOT NULL,
+  `idProduit` int NOT NULL,
+  `dateE` date NOT NULL,
+  `quantite` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `favori`
+--
+
+CREATE TABLE `favori` (
+  `id` int NOT NULL,
+  `idProduit` int NOT NULL,
+  `idUtilisateur` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fournisseur`
+--
+
+CREATE TABLE `fournisseur` (
+  `id` int NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `image`
+--
+
+CREATE TABLE `image` (
+  `id` int NOT NULL,
+  `idProduit` int NOT NULL,
+  `url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE `produit` (
+  `id` int NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `prix` float NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `idSousCategorie` int NOT NULL,
+  `stock` int NOT NULL,
+  `stockMinimum` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recherche`
+--
+
+CREATE TABLE `recherche` (
+  `id` int NOT NULL,
+  `idUtilisateur` int NOT NULL,
+  `motCle` varchar(255) NOT NULL,
+  `dateR` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `slide`
+--
+
+CREATE TABLE `slide` (
+  `id` int NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sousCategorie`
+--
+
+CREATE TABLE `sousCategorie` (
+  `id` int NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `idCategorie` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `id` int NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `dateInscription` date NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `motDePasse` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `visite`
+--
+
+CREATE TABLE `visite` (
+  `id` int NOT NULL,
+  `idProduit` int NOT NULL,
+  `idUtilisateur` int NOT NULL,
+  `dateV` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `administrateur`
+--
+ALTER TABLE `administrateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `adresseLivraison`
+--
+ALTER TABLE `adresseLivraison`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkAdresseLivraisonUtilisateur` (`idUtilisateur`) USING BTREE;
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkCommandeUtilisateur` (`idUtilisateur`),
+  ADD KEY `fkCommandeAdresseLivraison` (`idAdresse`);
+
+--
+-- Index pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkProduitCommentaire` (`idProduit`),
+  ADD KEY `fkUtilisateurCommentaire` (`idUtilisateur`);
+
+--
+-- Index pour la table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkUtilisateurContact` (`idUtilisateur`);
+
+--
+-- Index pour la table `coordonnee`
+--
+ALTER TABLE `coordonnee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `detailsCommande`
+--
+ALTER TABLE `detailsCommande`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkProduitDetailsCommande` (`idProduit`),
+  ADD KEY `fkCommandeDetailsCommande` (`idCommande`) USING BTREE;
+
+--
+-- Index pour la table `entreeEnStock`
+--
+ALTER TABLE `entreeEnStock`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkFournisseurEntreeEnStock` (`idFournisseur`),
+  ADD KEY `fkProduitEntreeEnStock` (`idProduit`);
+
+--
+-- Index pour la table `favori`
+--
+ALTER TABLE `favori`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idProduitFavori` (`idProduit`),
+  ADD KEY `fkUtilisateurFavori` (`idUtilisateur`);
+
+--
+-- Index pour la table `fournisseur`
+--
+ALTER TABLE `fournisseur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkProduitImage` (`idProduit`);
+
+--
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkSousCategorieProduit` (`idSousCategorie`);
+
+--
+-- Index pour la table `recherche`
+--
+ALTER TABLE `recherche`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkUtilisateurRecherche` (`idUtilisateur`) USING BTREE;
+
+--
+-- Index pour la table `slide`
+--
+ALTER TABLE `slide`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `sousCategorie`
+--
+ALTER TABLE `sousCategorie`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkSousCategorieCategorie` (`idCategorie`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `visite`
+--
+ALTER TABLE `visite`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkProduitVisite` (`idProduit`),
+  ADD KEY `fkUtilisateurVisite` (`idUtilisateur`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `administrateur`
+--
+ALTER TABLE `administrateur`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `adresseLivraison`
+--
+ALTER TABLE `adresseLivraison`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `commande`
+--
+ALTER TABLE `commande`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `coordonnee`
+--
+ALTER TABLE `coordonnee`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `detailsCommande`
+--
+ALTER TABLE `detailsCommande`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `entreeEnStock`
+--
+ALTER TABLE `entreeEnStock`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `favori`
+--
+ALTER TABLE `favori`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `fournisseur`
+--
+ALTER TABLE `fournisseur`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `image`
+--
+ALTER TABLE `image`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `produit`
+--
+ALTER TABLE `produit`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `recherche`
+--
+ALTER TABLE `recherche`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `slide`
+--
+ALTER TABLE `slide`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `sousCategorie`
+--
+ALTER TABLE `sousCategorie`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `visite`
+--
+ALTER TABLE `visite`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `adresseLivraison`
+--
+ALTER TABLE `adresseLivraison`
+  ADD CONSTRAINT `fkadresseLivraisonUtilisateur` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `fkCommandeAdresseLivraison` FOREIGN KEY (`idAdresse`) REFERENCES `adresseLivraison` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fkCommandeUtilisateur` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD CONSTRAINT `fkProduitCommentaire` FOREIGN KEY (`idProduit`) REFERENCES `produit` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fkUtilisateurCommentaire` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `contact`
+--
+ALTER TABLE `contact`
+  ADD CONSTRAINT `fkUtilisateurContact` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `detailsCommande`
+--
+ALTER TABLE `detailsCommande`
+  ADD CONSTRAINT `fkCommandeDetailCommande` FOREIGN KEY (`idCommande`) REFERENCES `commande` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fkProduitDetailsCommande` FOREIGN KEY (`idProduit`) REFERENCES `produit` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `entreeEnStock`
+--
+ALTER TABLE `entreeEnStock`
+  ADD CONSTRAINT `fkFournisseurEntreeEnStock` FOREIGN KEY (`idFournisseur`) REFERENCES `fournisseur` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fkProduitEntreeEnStock` FOREIGN KEY (`idProduit`) REFERENCES `produit` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `favori`
+--
+ALTER TABLE `favori`
+  ADD CONSTRAINT `fkUtilisateurFavori` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `idProduitFavori` FOREIGN KEY (`idProduit`) REFERENCES `produit` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `image`
+--
+ALTER TABLE `image`
+  ADD CONSTRAINT `fkProduitImage` FOREIGN KEY (`idProduit`) REFERENCES `produit` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD CONSTRAINT `fkSousCategorieProduit` FOREIGN KEY (`idSousCategorie`) REFERENCES `sousCategorie` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `recherche`
+--
+ALTER TABLE `recherche`
+  ADD CONSTRAINT `fkUtilisateur` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `sousCategorie`
+--
+ALTER TABLE `sousCategorie`
+  ADD CONSTRAINT `fkSousCategorieCategorie` FOREIGN KEY (`idCategorie`) REFERENCES `categorie` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `visite`
+--
+ALTER TABLE `visite`
+  ADD CONSTRAINT `fkProduitVisite` FOREIGN KEY (`idProduit`) REFERENCES `produit` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fkUtilisateurVisite` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
